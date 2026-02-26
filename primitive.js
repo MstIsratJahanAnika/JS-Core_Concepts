bccc2dd5bde24f3b0ecf1b86cf43d9858b2a476f
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-02-27 01:29:19 +0600

    primitive & non-primitive data types

---
+++ primitive.js
@@ -0,0 +1,12 @@
+let data = 27;
+data = 'kharmuja';
+data = {name: 'anika panika', 'thake-kotay': 'pilkhanay'};
+data = true; // boolean man 
+data = []; //array object hishebe count hoy 
+console.log(typeof data);
+console.log(Array.isArray(data)); //array kina confirm hoar jonno 
+
+// primitive - basic data types - uporer gula jemn 
+// non-primitive -> array, object, function eigula, reference wise type changes 
+
+// javascript dynamically typed
