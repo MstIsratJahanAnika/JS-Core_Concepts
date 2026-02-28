ff911a07dd262c7bc3b17881979aba44c4e195e3
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-02-28 14:44:49 +0600

    increment-decrement

---
+++ increment-decreament.js
@@ -0,0 +1,11 @@
+let a = 5; 
+a++; //a = a+1;
+console.log(a); // 6 
+console.log(a++); //post increment-previous value return kore increment hobe 
+console.log(a); //incremented value print 
+
+
+let b = 10;
+--b; //pre-decrement 9
+console.log(--b); // again pre-decrement 8
+console.log(b--); //post decrement print then decrease 8
